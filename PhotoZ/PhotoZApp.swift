0adf191e80f6902fc020a30609a3018d7dad94b5
ankurjha83 <ankur.jha@gmail.com>
2025-08-13 20:59:20 +0900

    Initial Commit

---
+++ PhotoZ/PhotoZApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoZApp.swift
+//  PhotoZ
+//
+//  Created by Swati Pareek on 13/08/25.
+//
+
+import SwiftUI
+
+@main
+struct PhotoZApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
